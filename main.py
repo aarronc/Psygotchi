82aae9853d80486dceebafb0a4d2cd31f851e5c1
aarronc <aarronc@gmail.com>
2023-09-24 17:41:30 +0100

    corrected path definitions for lib and pic

--- main.py
+++ main.py
@@ -4,8 +4,8 @@
 import sys
 import os
 import hashlib
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
